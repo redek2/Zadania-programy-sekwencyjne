c3d12b3a03f8918ee9e3fa13a0b57620e3016a22
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-24 01:55:43 +0200

    Add files via upload

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wysokość trójkąta:");
+        int wysokosc = int.Parse(Console.ReadLine());
+
+        int szerokosc = 1;
+        int spacje = wysokosc - 1;
+
+        for (int i = 0; i < wysokosc; i++)
+        {
+            // Rysuj spacje na początku wiersza
+            for (int j = 0; j < spacje; j++)
+            {
+                Console.Write(" ");
+            }
+
+            // Rysuj znaki "+" w wierszu
+            for (int j = 0; j < szerokosc; j++)
+            {
+                Console.Write("+");
+            }
+
+            Console.WriteLine();
+
+            // Zmniejsz liczbę spacji i zwiększ szerokość dla kolejnego wiersza
+            spacje--;
+            szerokosc += 2;
+        }
+    }
+}
